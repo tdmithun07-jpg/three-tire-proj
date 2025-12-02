9a10123f5cf31717557e0b7888a48974324891d2
tdmithun07-jpg <tdmithun.07@gmail.com>
2025-12-02 11:56:48 +0530

    nsg added

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -19,20 +19,8 @@ module "my_db_subnet" {
   source = "github.com/tdmithun07-jpg/three-tire-proj/modules/subnet/db-subnet"
 }
 
-# module "nsg" {
-#   source = "../../modules/nsg"
-# }
+module "nsg" {
+  source = "github.com/tdmithun07-jpg/three-tire-proj/modules/nsg"
+}
 
-# resource "azurerm_public_ip" "public_ip" {
-#   name                = "public_ip"
-#   resource_group_name = module.my_resource_group.resource_group_name
-#   location            = module.my_resource_group.location
-#   allocation_method   = "Dynamic"
-# }
 
-# resource "azurerm_public_ip" "private_ip" {
-#   name                = "private_ip"
-#   resource_group_name = module.my_resource_group.resource_group_name
-#   location            = module.my_resource_group.location
-#   allocation_method   = "Dynamic"
-# }
