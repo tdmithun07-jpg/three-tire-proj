29c05cc9cd84e3e29add2c7f3ee451ec8b8a5602
tdmithun07-jpg <tdmithun.07@gmail.com>
2025-12-03 15:06:52 +0530

    rg nd vnet

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -2,34 +2,33 @@ module "my_resource_group" {
     source    = "github.com/tdmithun07-jpg/three-tire-proj/modules/rg"
   }
 
-
 module "my_virtual_network" {
     source              = "github.com/tdmithun07-jpg/three-tire-proj/modules/vnet"
   }
 
-module "my_public_subnet" {
-    source              = "github.com/tdmithun07-jpg/three-tire-proj/modules/subnet/public-subnet"
-  }
+# module "my_public_subnet" {
+#     source              = "github.com/tdmithun07-jpg/three-tire-proj/modules/subnet/public-subnet"
+#   }
 
-module "my_private_subnet" {
-    source              = "github.com/tdmithun07-jpg/three-tire-proj/modules/subnet/private-subnet"
-  }
+# module "my_private_subnet" {
+#     source              = "github.com/tdmithun07-jpg/three-tire-proj/modules/subnet/private-subnet"
+#   }
 
-module "my_db_subnet" {
-  source = "github.com/tdmithun07-jpg/three-tire-proj/modules/subnet/db-subnet"
-}
+# module "my_db_subnet" {
+#   source = "github.com/tdmithun07-jpg/three-tire-proj/modules/subnet/db-subnet"
+# }
 
-module "nsg" {
-  source = "github.com/tdmithun07-jpg/three-tire-proj/modules/nsg"
-}
+# module "nsg" {
+#   source = "github.com/tdmithun07-jpg/three-tire-proj/modules/nsg"
+# }
 
-module "nic" {
-  source = "github.com/tdmithun07-jpg/three-tire-proj/modules/nic"
-  subnet_id = module.my_public_subnet.subnet_id
-}
+# module "nic" {
+#   source = "github.com/tdmithun07-jpg/three-tire-proj/modules/nic"
+#   # subnet_id = module.my_public_subnet.subnet_id
+# }
 
-module "vm" {
-  source = "github.com/tdmithun07-jpg/three-tire-proj/modules/vm"
-  network_interface_name = module.nic.network_interface_ids
-  subnet_id = module.my_public_subnet.subnet_id
-}
+# # module "vm" {
+# #   source = "github.com/tdmithun07-jpg/three-tire-proj/modules/vm"
+# #   network_interface_name = module.nic.network_interface_ids
+# #   subnet_id = module.my_public_subnet.subnet_id
+# # }
