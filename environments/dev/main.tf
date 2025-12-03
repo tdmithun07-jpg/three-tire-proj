4b5e04ac7249794cbc2051a464e274fc8b9c234c
tdmithun07-jpg <tdmithun.07@gmail.com>
2025-12-03 10:55:20 +0530

    vm nic removed

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -31,5 +31,4 @@ module "nic" {
 module "vm" {
   source = "github.com/tdmithun07-jpg/three-tire-proj/modules/vm"
   subnet_id = module.my_public_subnet.subnet_id
-  network_interface_ids = [module.nic.network_interface_ids]
 }
