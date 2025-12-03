6d538fc1f5f09d5f27dd10807d690e1ae9f0e37e
tdmithun07-jpg <tdmithun.07@gmail.com>
2025-12-03 19:30:40 +0530

    vnet outputs edited

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -12,7 +12,7 @@ module "my_subnets" {
     source              = "github.com/tdmithun07-jpg/three-tire-proj/modules/subnet"
     resource_group_name = module.my_resource_group.resource_group_name
     location = module.my_resource_group.location
-    virtual_network_name = module.my_virtual_network.virtual_network_name
+    vnet_name = module.my_virtual_network.virtual_network_name
   }
 
 
--- modules/vnet/outputs.tf
+++ modules/vnet/outputs.tf
@@ -1,7 +1,7 @@
-output "vent_address_space" {
-  value = azurerm_virtual_network.vnet.address_space
-}
-
 output "virtual_network_name" {
   value = azurerm_virtual_network.vnet.name
 }
+
+# output "address_space" {
+#   value = azurerm_virtual_network.vnet.address_space
+# }
