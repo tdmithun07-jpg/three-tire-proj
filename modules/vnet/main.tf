632a1fa398749a2cdde35b8256686a1464d9e94b
tdmithun07-jpg <tdmithun.07@gmail.com>
2025-12-03 16:32:12 +0530

    module paths in vnet 5

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_network" "vnet" {
     name                = var.vnet_name
     address_space       = var.address_space
-    resource_group_name = module.rg.name
-    location            = module.rg.location
+    resource_group_name = var.resource_group_name
+    location            =   var.location
 }
