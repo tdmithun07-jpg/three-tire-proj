752f660e81704d76e883b02afddc85466dabc82d
tdmithun07-jpg <tdmithun.07@gmail.com>
2025-12-07 23:25:51 +0530

    0712 vm added 8

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -1,5 +1,5 @@
 
-resource "azurerm_virtual_machine" "main" {
+resource "azurerm_virtual_machine" "vm" {
   name                  = var.virtual_machine_name
   location              = var.location
   resource_group_name   = var.resource_group_name
