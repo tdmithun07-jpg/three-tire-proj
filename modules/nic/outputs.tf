f6da0049ad80ae8e7a384bc427000be5e687f706
tdmithun07-jpg <tdmithun.07@gmail.com>
2025-12-07 23:19:49 +0530

    0712 vm added 7

--- modules/nic/outputs.tf
+++ modules/nic/outputs.tf
@@ -2,6 +2,10 @@ output "network_interface_ids" {
   value = azurerm_network_interface.nic.id
 }
 
+output "network_interface_name" {
+  value = azurerm_network_interface.nic.name
+}
+
 # output "private_network_interface_ids" {
 #   value = azurerm_network_interface.private_nic.id
 # }
