1d2d26d5e1fe915343a6afdf30079cfef7919347
tdmithun07-jpg <tdmithun.07@gmail.com>
2025-12-08 16:44:44 +0530

    0712 private nic added 2

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -32,7 +32,7 @@ module "public_nic" {
 
 module "private_nic" {
   source = "github.com/tdmithun07-jpg/three-tire-proj/modules/nic"
-  private_network_interface_name = "private_nic"
+  network_interface_name = "private_nic"
   location = module.my_resource_group.location  
   resource_group_name =  module.my_resource_group.resource_group_name
   subnet_id = module.my_subnets.private_subnet_id
