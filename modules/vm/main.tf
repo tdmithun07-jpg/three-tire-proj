700c790656a1a68e9683ea09e71d90517008479b
tdmithun07-jpg <tdmithun.07@gmail.com>
2025-12-09 21:18:46 +0530

    0912 vm disk changed 1

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_virtual_machine" "vm" {
   location              = var.location
   resource_group_name   = var.resource_group_name
   network_interface_ids = [var.network_interface_ids]
-  vm_size               = "Standard_DS1_v2"
+  vm_size               = "Standard_B1s"
 
   # Uncomment this line to delete the OS disk automatically when deleting the VM
   delete_os_disk_on_termination = true
@@ -14,7 +14,7 @@ resource "azurerm_virtual_machine" "vm" {
   storage_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts-gen2"
+    sku       = "20_04-lts"
     version   = "latest"
   }
   storage_os_disk {
