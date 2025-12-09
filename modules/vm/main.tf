9ef3093fb276728a0e9cdbfc238e74b56a42f341
tdmithun07-jpg <tdmithun.07@gmail.com>
2025-12-09 15:24:00 +0530

    0912 nic path changed in vm

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_virtual_machine" "vm" {
   name                  = var.virtual_machine_name
   location              = var.location
   resource_group_name   = var.resource_group_name
-  network_interface_ids = [var.network_interface_name]
+  network_interface_ids = [var.network_interface_ids]
   vm_size               = "Standard_DC1s_v3"
 
   # Uncomment this line to delete the OS disk automatically when deleting the VM
