264362a917dc296e327d9d6bd79d1328a64f61db
tdmithun07-jpg <tdmithun.07@gmail.com>
2025-12-10 23:52:57 +0530

    1012 public ip block added 4

---
+++ modules/publicip/variables.tf
@@ -0,0 +1,14 @@
+variable "public_ip_name" {
+  description = "name of the public ip"
+  type = string
+}
+
+variable "resource_group_name" {
+  description = "rg name"
+  type = string
+}
+
+variable "location" {
+  description = "region"
+  type = string
+}
