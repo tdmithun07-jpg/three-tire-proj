83dad217f475a302d4991e9344e8bf8e8130d12a
tdmithun07-jpg <tdmithun.07@gmail.com>
2025-12-11 16:58:48 +0530

    1112 public ip  module changed

--- modules/publicip/main.tf
+++ modules/publicip/main.tf
@@ -5,9 +5,9 @@ resource "azurerm_public_ip" "web-ip" {
   allocation_method   = "Dynamic"
 }
 
-resource "azurerm_public_ip" "app-ip" {
-  name                = var.app_ip_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  allocation_method   = "Dynamic"
-}
+# resource "azurerm_public_ip" "app-ip" {
+#   name                = var.app_ip_name
+#   location            = var.location
+#   resource_group_name = var.resource_group_name
+#   allocation_method   = "Dynamic"
+# }
--- modules/publicip/outputs.tf
+++ modules/publicip/outputs.tf
@@ -2,6 +2,6 @@ output "web_ip_address_id" {
   value = azurerm_public_ip.web-ip.id
 }
 
-output "app_ip_address_id" {
-  value = azurerm_public_ip.app-ip.id
-}
+# output "app_ip_address_id" {
+#   value = azurerm_public_ip.app-ip.id
+# }
