16c5fce936aa40c9140209e56b224febbaa88c48
tdmithun07-jpg <tdmithun.07@gmail.com>
2025-12-11 17:49:44 +0530

    1112 public ip  module changed and allocation changed to static

--- modules/publicip/main.tf
+++ modules/publicip/main.tf
@@ -2,8 +2,8 @@ resource "azurerm_public_ip" "web-ip" {
   name                = var.web_ip_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  allocation_method   = "Dynamic"
-  sku = "Basic"
+  allocation_method   = "Static"
+  #sku = "Basic"
 }
 
 # resource "azurerm_public_ip" "app-ip" {
