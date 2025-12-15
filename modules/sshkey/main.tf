fd8c488cb8af9cf56ceacb81c4675336adc031f2
tdmithun07-jpg <tdmithun.07@gmail.com>
2025-12-15 10:22:37 +0530

    1312 ssh key module added 11

--- modules/sshkey/main.tf
+++ modules/sshkey/main.tf
@@ -3,13 +3,13 @@ resource "tls_private_key" "vm1" {
   rsa_bits  = 4096
 }
 
-resource "local_file" "private_key" {
+resource "local_file" "pri_key" {
   content         = tls_private_key.vm1.private_key_pem
   filename        = pathexpand("github.com/tdmithun07-jpg/three-tire-proj/environments/dev/keys/vm1") # Save private key as ~/.ssh/vm1
   #file_permission = "0600"
 }
 
-resource "local_file" "public_key" {
+resource "local_file" "pub_key" {
   content  = tls_private_key.vm1.public_key_openssh
   filename = pathexpand("github.com/tdmithun07-jpg/three-tire-proj/environments/dev/keys/vm1.pub") # Save public key as ~/.ssh/vm1.pub
 }
