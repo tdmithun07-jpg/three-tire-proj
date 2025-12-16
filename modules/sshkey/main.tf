631388f79cf97ac5ee69544dfca6ec185522ce5e
tdmithun07-jpg <tdmithun.07@gmail.com>
2025-12-16 17:30:16 +0530

    1612 sshkey module edited

--- modules/sshkey/main.tf
+++ modules/sshkey/main.tf
@@ -1,18 +1,18 @@
-resource "tls_private_key" "vm1" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
+# resource "tls_private_key" "vm1" {
+#   algorithm = "RSA"
+#   rsa_bits  = 4096
+# }
 
-resource "local_file" "pri_key" {
-  content         = tls_private_key.vm1.private_key_pem
-  filename        = pathexpand("github.com/tdmithun07-jpg/three-tire-proj/environments/dev/keys/vm1") # Save private key as ~/.ssh/vm1
-  #file_permission = "0600"
-}
+# resource "local_file" "pri_key" {
+#   content         = tls_private_key.vm1.private_key_pem
+#   filename        = pathexpand("github.com/tdmithun07-jpg/three-tire-proj/environments/dev/keys/vm1") # Save private key as ~/.ssh/vm1
+#   #file_permission = "0600"
+# }
 
-resource "local_file" "pub_key" {
-  content  = tls_private_key.vm1.public_key_openssh
-  filename = pathexpand("github.com/tdmithun07-jpg/three-tire-proj/environments/dev/keys/vm1.pub") # Save public key as ~/.ssh/vm1.pub
-}
+# resource "local_file" "pub_key" {
+#   content  = tls_private_key.vm1.public_key_openssh
+#   filename = pathexpand("github.com/tdmithun07-jpg/three-tire-proj/environments/dev/keys/vm1.pub") # Save public key as ~/.ssh/vm1.pub
+# }
 
 # resource "azurerm_ssh_public_key" "pub_key" {
 #   name                = "pub_key"
@@ -21,7 +21,7 @@ resource "local_file" "pub_key" {
 #   public_key          = file(pathexpand(var.public_key_path))
 # }
 
-# data "local_file" "ssh_key" { 
-#     content = 
-#     filename = "github.com/tdmithun07-jpg/three-tire-proj/environments/dev/keys/testkey.txt"
-#  }
+data "local_file" "ssh_key" { 
+    #content = file("github.com/tdmithun07-jpg/three-tire-proj/environments/dev/keys/testkey.txt")
+    filename = "git::https://github.com/tdmithun07-jpg/three-tire-proj/environments/dev/keys/testkey.txt"
+ }
--- modules/sshkey/outputs.tf
+++ modules/sshkey/outputs.tf
@@ -1,15 +1,15 @@
-output "private_key_pem" {
-  value = tls_private_key.vm1.private_key_pem
-}
+# output "private_key_pem" {
+#   value = tls_private_key.vm1.private_key_pem
+# }
 
-output "public_key_openssh" {
-  value = tls_private_key.vm1.public_key_openssh
-}
+# output "public_key_openssh" {
+#   value = tls_private_key.vm1.public_key_openssh
+# }
 
 # output "public_key_path" {
 #   value = azurerm_ssh_public_key.pub_key.public_key
 # }
 
-# output "public_key_path" {
-#   value = data.local_file.ssh_key.filename
-# }
+output "public_key_path" {
+  value = data.local_file.ssh_key.content
+}
