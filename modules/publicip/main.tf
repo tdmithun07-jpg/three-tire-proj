ede060dcb9188f9a6524b81062f0ee18faf271f7
tdmithun07-jpg <tdmithun.07@gmail.com>
2025-12-20 23:49:04 +0530

    2012 app vm added 3

--- modules/publicip/main.tf
+++ modules/publicip/main.tf
@@ -6,9 +6,9 @@ resource "azurerm_public_ip" "web-ip" {
   #sku = "Basic"
 }
 
-# resource "azurerm_public_ip" "app-ip" {
-#   name                = var.app_ip_name
-#   location            = var.location
-#   resource_group_name = var.resource_group_name
-#   allocation_method   = "Dynamic"
-# }
+resource "azurerm_public_ip" "app-ip" {
+  name                = var.app_ip_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Dynamic"
+}
