300772a79e96a8b470e9423450209faaf2af42e5
tdmithun07-jpg <tdmithun.07@gmail.com>
2025-12-22 02:01:31 +0530

    2122 storage account added 4

--- modules/storageaccount/main.tf
+++ modules/storageaccount/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "statefile-storage" {
-  name                     = "state file storage"
+  name                     = "statefilestorage2212"
   resource_group_name      = var.resource_group_name
   location                 = var.location
   account_tier             = "Standard"
