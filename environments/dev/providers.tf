0b22d1e07888521b5fd9e6a593c67bcffe934464
tdmithun07-jpg <tdmithun.07@gmail.com>
2025-12-23 00:00:30 +0530

    test reset 2

--- environments/dev/providers.tf
+++ environments/dev/providers.tf
@@ -7,21 +7,21 @@ terraform {
     }
   }
 
-  # cloud {
-  #   organization = "three-tire-proj"
-  #   workspaces {
-  #     name = "three-tire-project"
-  #   }
-  #  }
-  # }
-
-backend "azurerm" {
-    resource_group_name  = "dev-rg"
-    storage_account_name = "statefilestorage2212"
-    container_name       = "statefilecontainer"
-    key                  = "terraform.tfstate"
+  cloud {
+    organization = "three-tire-proj"
+    workspaces {
+      name = "three-tire-project"
+    }
+   }
   }
-}
+
+# backend "azurerm" {
+#     resource_group_name  = "dev-rg"
+#     storage_account_name = "statefilestorage2212"
+#     container_name       = "statefilecontainer"
+#     key                  = "terraform.tfstate"
+#   }
+# }
 
 provider "azurerm" {
   subscription_id = "513cd041-2e87-42df-990d-02ccf836f17d"
